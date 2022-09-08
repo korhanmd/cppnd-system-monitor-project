06af58d96b63cc2603eac33d3c6237816616bf4c
Korhan Mutludoğan <korhanmd@gmail.com>
2022-09-08 21:49:03 +0300

    Read and return the number of running processes

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -140,8 +140,23 @@ int LinuxParser::TotalProcesses() {
   return 0;
 }
 
-// TODO: Read and return the number of running processes
-int LinuxParser::RunningProcesses() { return 0; }
+// Read and return the number of running processes
+int LinuxParser::RunningProcesses() {
+  string line, key, value;
+
+  std::ifstream filestream(kProcDirectory + kStatFilename);
+  
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      linestream >> key >> value;
+
+      if (key == "procs_running") return std::stoi(value);
+    }
+  }
+  
+  return 0;
+}
 
 // TODO: Read and return the command associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
