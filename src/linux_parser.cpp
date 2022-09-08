b21243ee541e36ea86d23401134a154d3de1e5df
Korhan Mutludoğan <korhanmd@gmail.com>
2022-09-08 21:42:05 +0300

    Read and return the total number of processes

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -122,8 +122,23 @@ long LinuxParser::IdleJiffies() { return 0; }
 // TODO: Read and return CPU utilization
 vector<string> LinuxParser::CpuUtilization() { return {}; }
 
-// TODO: Read and return the total number of processes
-int LinuxParser::TotalProcesses() { return 0; }
+// Read and return the total number of processes
+int LinuxParser::TotalProcesses() {
+  string line, key, value;
+
+  std::ifstream filestream(kProcDirectory + kStatFilename);
+  
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      linestream >> key >> value;
+
+      if (key == "processes") return std::stoi(value);
+    }
+  }
+
+  return 0;
+}
 
 // TODO: Read and return the number of running processes
 int LinuxParser::RunningProcesses() { return 0; }
