5795bbe49d5abb21455c70f8d26e7f56d7c7aad9
Korhan Mutludoğan <korhanmd@gmail.com>
2022-09-10 22:56:05 +0300

    Return the command that generated the process

--- include/process.h
+++ include/process.h
@@ -11,7 +11,7 @@ class Process {
   Process(int pid) : pid_{pid} {}
   int Pid();
   std::string User();
-  std::string Command();                   // TODO: See src/process.cpp
+  std::string Command();
   float CpuUtilization();                  // TODO: See src/process.cpp
   std::string Ram();                       // TODO: See src/process.cpp
   long int UpTime();                       // TODO: See src/process.cpp
